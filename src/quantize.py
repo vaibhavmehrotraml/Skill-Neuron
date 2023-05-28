8f537e0e144561e5928307b0d239dbabc16543cf
Vaibhav Mehrotra <vaibhav1@uw.edu>
2023-05-28 16:47:08 -0700

    Rollback

--- src/quantize.py
+++ src/quantize.py
@@ -4,4 +4,7 @@ from trainer import *
 def main():
     roberta = AutoModelForMaskedLM.from_pretrained("roberta-base")
     state_dict = roberta.state_dict()
+
+    print(type(roberta))
+    print(type(state_dict))
     pass
